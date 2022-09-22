92024bb6acfd639971d828140d14726e52cbde44
Anderson Leony <leony_maia@edu.unifor.br>
2022-09-21 22:06:24 -0300

    Correção da função contagem

--- entregavel1_java/classes/F_Contagem.java
+++ entregavel1_java/classes/F_Contagem.java
@@ -8,11 +8,11 @@ public class F_Contagem {
         Scanner sc1 = new Scanner(System.in);
         String arrayStr = "";
 
-        System.out.println("Digite um número e veja quantos valores inteiros existem entre 1 (inclusive) e o num. que vc digitou (inclusive):");
+        System.out.println("Digite um número e veja quantos valores inteiros existem entre 1 (inclusive) e o num. que vc digitou:");
         int n = sc1.nextInt();
 
         for (int i = 0; i < n; i++) {
-            System.out.println("Informe o "+(i+1)+"° num (de 5): ");
+            System.out.println("Informe o "+(i+1)+"° num (de "+n+"): ");
             String aux = sc1.next();
             arrayStr = arrayStr + aux;
             if(i!=n-1) {
@@ -24,20 +24,24 @@ public class F_Contagem {
         int totalOfNumbers = 0;
         
         for (int index = 0; index < array.length; index++) {
-            if(isInterger(array[index])) {
+            if(isInterger(array[index], n)) {
                 totalOfNumbers++;
             }
         }
         return "Total de números inteiros = "+totalOfNumbers;
     }
 
-    public static boolean isInterger(String value) {
+    public static boolean isInterger(String value, int n) {
         if(!value.contains(".")){
-            return true;
+            if(Integer.parseInt(value)<=n) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
             String[] decimalValue = value.split("\\.");
 
-            if(Integer.parseInt(decimalValue[1])==0) {
+            if(Integer.parseInt(decimalValue[0])<=n && Integer.parseInt(decimalValue[1])==0) {
                 return true;
             } else {
                 return false;
--- entregavel1_javascript/js/f_contagem.js
+++ entregavel1_javascript/js/f_contagem.js
@@ -1,14 +1,12 @@
 function count() {
-    let n = prompt("Digite um número e veja quantos valores inteiros existem entre 1 (inclusive) e o num. que vc digitou (inclusive):")
+    let n = prompt("Digite um número e veja quantos valores inteiros existem entre 1 (inclusive) e o num. que vc digitou:")
 
     let array = ((prompt(`Agora escreva os ${n} números separados por virgula ( Ex.: 1,2,3.5,4 )`)).split(","));
     
     let totalOfNumbers = 0;
 
-    console.log(array)
-
     for (let index = 0; index < array.length; index++) {
-        if(isInterger(array[index])) {
+        if(isInterger(array[index], n)) {
             totalOfNumbers++;
         }
     }
@@ -16,13 +14,17 @@ function count() {
     alert(`Total de números inteiros: ${totalOfNumbers}`);
 }
 
-function isInterger(strValue = "") {
+function isInterger(strValue = "", n = "") {
     if(!strValue.includes(".")){
-        return true;
+        if(parseInt(strValue)<=n) {
+            return true;
+        } else {
+            return false;
+        }
     } else {
         let decimalValue = strValue.split("\\.");
 
-        if(parseInt(decimalValue[1])==0) {
+        if(parseInt(decimalValue[0])<=n && parseInt(decimalValue[1])==0) {
             return true;
         } else {
             return false;
